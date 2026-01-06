cdc46733e3bc82b16bd1529b9ba31483f2b1a93b
harish-08-creator <83396752+harish-08-creator@users.noreply.github.com>
2026-01-06 09:31:57 +0530

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+//configuration of environment variables
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(rateLimiter);
+app.use(express.json()); //for accessing request body
+
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT: " + PORT);
+  });
+});
